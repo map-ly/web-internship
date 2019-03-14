45699f219d897431ea03ec37da86fe040de7594b
lymap <lymap6@gmail.com>
2019-03-14 16:33:48 +0700

    update

---
+++ web/booking.php
@@ -0,0 +1,137 @@
+<!--
+author: W3layouts
+author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Grand Tour Travel Category Flat Bootstrap Responsive Web Template | Booking :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<meta name="keywords" content="Grand Tour Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+
+    <script>
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+	
+	<!-- css files -->
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css' /><!-- bootstrap css -->
+    <link href="css/style.css" rel='stylesheet' type='text/css' /><!-- custom css -->
+    <link href="css/font-awesome.min.css" rel="stylesheet"><!-- fontawesome css -->
+	<!-- //css files -->
+	
+	<!-- google fonts -->
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+	<!-- //google fonts -->
+	
+</head>
+<body>
+
+
+<!-- //header -->
+
+<!-- banner -->
+<section class="banner_inner" id="home">
+	<div class="banner_inner_overlay">
+	</div>
+</section>
+<!-- //banner -->
+
+
+<!-- Booking -->
+<section class="contact py-5">
+	<div class="container py-lg-5 py-sm-4">
+		<h2 class="heading text-capitalize text-center mb-lg-5 mb-4"> Book Your Tour</h2>
+		<div class="contact-grids">
+			<div class="row">
+				<div class="col-lg-7 contact-left-form">
+					<form action="#" method="post" class="row">
+						<div class="col-sm-6 form-group contact-forms">
+						  <input type="text" class="form-control" placeholder="Name" required="">
+						</div>
+						<div class="col-sm-6 form-group contact-forms">
+						  <input type="email" class="form-control" placeholder="Email" required="">
+						</div>
+						<div class="col-sm-6 form-group contact-forms">
+						  <input type="text" class="form-control" placeholder="Phone" required=""> 
+						</div>
+						<div class="col-sm-6 form-group contact-forms">
+						  <input type="text" class="form-control" placeholder="Date" required=""> 
+						</div>
+						<div class="col-sm-6 form-group contact-forms">
+							<select class="form-control" id="adults">
+								<option>Adults</option>
+								<option>1</option>
+								<option>2</option>
+								<option>3</option>
+								<option>4</option>
+								<option>5 or more</option>
+							</select>
+						</div>
+						<div class="col-sm-6 form-group contact-forms">
+							<select class="form-control" id="kids">
+								<option>Kids</option>
+								<option>1</option>
+								<option>2</option>
+								<option>3</option>
+								<option>4</option>
+								<option>5 or more</option>
+							</select>
+						</div>
+						<div class="col-md-12 form-group contact-forms">
+						  <textarea class="form-control" placeholder="Message" rows="3" required=""></textarea>
+						</div>
+						<div class="col-md-12 booking-button">
+							<button class="btn btn-block sent-butnn">Book Now</button>
+						</div>
+					</form>
+				</div>
+				<div class="col-lg-5 contact-right pl-lg-5">
+				
+					<div class="image-tour position-relative">
+						<img src="images/banner1.jpg" alt="" class="img-fluid" />
+						<p><span class="fa fa-tags"></span> <span>20$ - 15% off</span></p>
+					</div>
+					
+					<h4>Tour Description</h4>
+					<p class="mt-3">Duis nisi sapien, elementum finibus ferme ntum ed eget, aliquet et leo. Mauris hendrerit vel ex.
+					vitae luctus massa. Phas ellus sed aliquam leo et dolor. Vestibulum ullamcorper massa eut sed fringilla.</p>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- //Booking -->
+
+
+
+<!-- //footer -->
+
+<!-- copyright -->
+<div class="copyright py-3 text-center">
+	<p>© 2019 Grand Tour. All Rights Reserved | Design by <a href="http://w3layouts.com/" target="=_blank"> W3layouts </a></p>
+</div>
+<!-- //copyright -->
+
+<!-- move top -->
+<div class="move-top text-right">
+	<a href="#home" class="move-top"> 
+		<span class="fa fa-angle-up  mb-3" aria-hidden="true"></span>
+	</a>
+</div>
+<!-- move top -->
+
+	
+</body>
+</html>
