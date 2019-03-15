c61daff21f64b4663f867f385294cb3c3bdb6ae2
lymap <lymap6@gmail.com>
2019-03-15 08:21:39 +0700

    update

--- web/css/index.php
+++ web/css/index.php
@@ -11,22 +11,6 @@
 <?php include('footer.php'); ?>
 
 
-
-
-
-
-<?php
-	/*if(isset($_GET['page'])){
-		$page = $_GET['page'];
-		if (!empty($page)) {
-			$page .= '.php';
-			include($page);
-		}
-		else {
-			include('home.php');
-		} 
-	}else{
-		include('home.php');
-	}
 ?>
-<?php include('footer.php'); ?>*/
+
+
