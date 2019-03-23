a3e8d0b93712358ec54dc18f288c1bde73c50138
lymap <lymap6@gmail.com>
2019-03-23 08:53:59 +0700

    update

---
+++ phpadvanced/filter.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$str = "<h1>Hello World!</h1>";
+$newstr = filter_var($str, FILTER_SANITIZE_STRING);
+echo $newstr;
+
+$int = 100;
+
+if (!filter_var($int, FILTER_VALIDATE_INT) === false) {
+    echo("Integer is valid");
+} else {
+    echo("Integer is not valid");
+}
+?>
+
+</body>
+</html>
